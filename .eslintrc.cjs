2564ed3422bf17bd69fcd58bd07ed988c62159d8
Manuel Lopez <51802110+Manuel-Lopez17@users.noreply.github.com>
2024-01-11 11:59:51 -0300

    lint TEST 2

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
