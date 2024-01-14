c5ddc2d90e98caf1d92d14284c9341b914046ebd
Manuel Lopez <manu1475963@gmail.com>
2024-01-14 12:09:34 -0300

    add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: "Manuel-Lopez17.github.io"
-})
+  devOptions: {
+    port: 3000,
+    hostname: '',
+    site: 'https://manuel-lopez17.github.io/',
+    base: 'manuel-lopez17.github.io'
+  },
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  })]
+});
