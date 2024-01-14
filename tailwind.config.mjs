21a947c946da2e045367417b0a276155ff5c05f6
Manuel Lopez <manu1475963@gmail.com>
2024-01-14 15:10:40 -0300

    add chage theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -66,6 +66,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontSize: {
+        clamp: "clamp(1rem, 5vw, 3rem)",
+      },
+      
     },
   },
   plugins: [require("tailwindcss-animate")],
