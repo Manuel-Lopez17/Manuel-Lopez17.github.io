e2c5245cedda784c679709bc52d4fad76c9df5fa
Manuel Lopez <51802110+Manuel-Lopez17@users.noreply.github.com>
2024-01-15 11:39:57 -0300

    add shadcn Components

---
+++ src/components/Nav.tsx
@@ -0,0 +1,32 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+export function Nav(){
+
+    return (
+       <div className="w-full flex flex-row justify-center items-center">
+
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+       </div> 
+
+    )
+}
+
+
